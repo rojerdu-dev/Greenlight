7338dd6ddb60f89da407e1da94d59bda84b866f0
Rojer Du <rojerdu.dev@gmail.com>
2024-04-02 16:04:01 -0700

    add envelope to JSON response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -4,14 +4,16 @@ import (
 	"net/http"
 )
 
-func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+func (app *application) healthcheckHandler(w http.ResponseWriter, _ *http.Request) {
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
