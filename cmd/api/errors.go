e5708d096d7012e9fc15ada2f23374ccbcde32e4
Rojer Du <rojerdu.dev@gmail.com>
2024-04-05 13:55:42 -0700

    update bad request response

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
 func (app *application) logError(r *http.Request, err error) {
 	var (
 		method = r.Method
