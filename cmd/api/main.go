d11a77ce0c417aaf8c7d7b19bc98f7958a5c7fa4
Rojer Du <rojerdu.dev@gmail.com>
2024-04-09 12:24:00 -0700

    set up Movie Model

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/rojerdu-dev/Greenlight/internal/data"
+
 	_ "github.com/lib/pq"
 )
 
@@ -31,6 +33,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -60,6 +63,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := http.Server{
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/rojerdu-dev/Greenlight
 go 1.22.1
 
 require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/golang-migrate/migrate/v4 v4.17.0
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.9
 )
