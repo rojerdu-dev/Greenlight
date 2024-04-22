f2bdabe4cae66e97173171d39c6260377211dc84
Rojer Du <rojerdu.dev@gmail.com>
2024-04-22 11:05:27 -0700

    add pagiation and return pagination metadata

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -6,6 +6,28 @@ import (
 	"github.com/rojerdu-dev/Greenlight/internal/validator"
 )
 
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     (totalRecords + pageSize - 1) / pageSize,
+		TotalRecords: totalRecords,
+	}
+}
+
 type Filters struct {
 	Page         int
 	PageSize     int
