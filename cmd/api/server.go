d83dfbc67e17af47b85c12447115c99b06c3e558
Rojer Du <rojerdu.dev@gmail.com>
2024-04-26 19:35:25 -0700

    add graceful shutdown in server.go

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -21,19 +23,29 @@ func (app *application) serve() error {
 		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
 
+	shutdownError := make(chan error)
+
 	go func() {
 		quit := make(chan os.Signal, 1)
-
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-
 		s := <-quit
 
-		app.logger.Info("caught signal", slog.String("signal", s.String()))
+		app.logger.Info("shutting down server", slog.String("signal", s.String()))
 
-		os.Exit(0)
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
 	}()
 
 	app.logger.Info("starting server", slog.String("addr", srv.Addr), slog.String("env", app.config.env))
 
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	app.logger.Info("stopped server", slog.String("addr", srv.Addr))
+
+	return nil
 }
