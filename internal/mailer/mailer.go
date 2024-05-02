d65677adc857d836217e44cd36e7ba73e28aecaa
Rojer Du <rojerdu.dev@gmail.com>
2024-05-02 04:43:43 -0700

    add resend email functionality

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -63,5 +63,14 @@ func (m Mailer) Send(recipient, templateFile string, data any) error {
 		return err
 	}
 
-	return nil
+	for i := 1; i <= 3; i++ {
+		err = m.dialer.DialAndSend(msg)
+		if nil == err {
+			return nil
+		}
+
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	return err
 }
