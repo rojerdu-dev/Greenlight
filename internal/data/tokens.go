d0fffd7cce1b68b26a6b1f48ac2bbcbbb39d0b36
Rojer Du <rojerdu.dev@gmail.com>
2024-05-06 14:33:31 -0700

    create secure activation tokens and activeUserHandler

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -67,7 +67,7 @@ func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, 
 func (m TokenModel) Insert(token *Token) error {
 	query := `
 		INSERT INTO tokens (hash, user_id, expiry, scope)
-		VALUES ($1, $2, $3)
+		VALUES ($1, $2, $3, $4)
     `
 
 	args := []any{token.Hash, token.UserID, token.Expiry, token.Scope}
--- internal/data/users.go
+++ internal/data/users.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"crypto/sha256"
 	"database/sql"
 	"errors"
 	"time"
@@ -86,6 +87,47 @@ type UserModel struct {
 	DB *sql.DB
 }
 
+func (m UserModel) GetForToken(tokenScope, tokenPlaintext string) (*User, error) {
+	tokenHash := sha256.Sum256([]byte(tokenPlaintext))
+
+	query := `
+		SELECT users.id, users.created_at, users.name, users.email, users.password_hash, users.activated, users.version
+		FROM users
+		INNER JOIN tokens
+		ON users.id = tokens.user_id
+		WHERE tokens.hash = $1
+		AND tokens.scope = $2
+		AND tokens.expiry > $3
+	`
+
+	args := []any{tokenHash[:], tokenScope, time.Now()}
+
+	var user User
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &user, nil
+}
+
 func (m UserModel) Insert(user *User) error {
 	query := `
 		INSERT INTO users (name, email, password_hash, activated)
@@ -113,7 +155,7 @@ func (m UserModel) Insert(user *User) error {
 
 func (m UserModel) GetByEmail(email string) (*User, error) {
 	query := `
-   		SELECT id, created_at, name, email, password_hash, activated, version
+		SELECT id, created_at, name, email, password_hash, activated, version
 		FROM users
 		WHERE email = $1
     `
